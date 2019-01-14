532a566cd5047b034acb909d1d553ec652a517b2
Aikol <imaikol@yahoo.com>
2019-01-13 22:22:15 -0800

    Update Detail.js

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,60 +1,53 @@
-import React from 'react';
+// import React from 'react';
 
-const Detail = ({phone})=>{
-  let click = () => alert(phone.model);
-  let booking = (e) => {
-    if(e.type === 'click'){
-      alert("Order is booked");
-    }
-  }
-  return(
-    <div>
-      <h3 onClick={click}>{phone.model}</h3>
-      <p><button onClick={booking}>book</button></p>
-    </div>
-  )
-}
-export default Detail;
+// const Detail = ({phone})=>{
+//   let click = () => alert(phone.model);
+//   let booking = (e) => {
+//     if(e.type === 'click'){
+//       alert("Order is booked");
+//     }
+//   }
+//   return(
+//     <div>
+//       <h3 onClick={click}>{phone.model}</h3>
+//       <p><button onClick={booking}>book</button></p>
+//     </div>
+//   )
+// }
+// export default Detail;
 /////////////////////////////////
 //
-// import React, { Component } from 'react';
-//
-// class Detail extends Component {
-//   constructor(props){
-//     super(props)
-//     this.state = {
-//       postItem: null
-//     }
-//   }
-//
-//   setPostStateOnProps(){
-//     const {phone} = this.props
-//     this.setState({
-//       postItem: phone
-//     })
-//   }
-//   componentDidUpdate(prevProps, prevState, snapshop){
-//     if(this.props !== prevProps){
-//       this.setPostStateOnProps()
-//     }
-//   }
-//   componentDidMount(){
-//     this.setPostStateOnProps()
-//   }
-//
-//   click = () => {
-//     console.log(phone.model);
-//   }
-//
-//   render() {
-//     const {postItem} = this.state;
-//     return (
-//       <div>
-//         <h1 onClick={this.click}>{phone.model}</h1>
-//       </div>
-//     );
-//   }
-//
-// }
-//
-// export default Detail;
+import React, { Component } from 'react';
+
+class Detail extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      active: false
+    }
+  }
+
+  click = () => {
+    this.setState({
+      active: !this.state.active
+    })
+    console.log(this.state.active);
+  }
+
+  render() {
+    const {phone} = this.props
+    const {active} = this.state;
+    return (
+      <div id="detail">
+        <h1>{phone.model}</h1>
+        {active === false ?
+        (<button onClick={this.click}>Book this phone</button>):
+        (<button onClick={this.click}>{phone.content}</button>)
+      }
+      </div>
+    );
+  }
+
+}
+
+export default Detail;
